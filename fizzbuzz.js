3a32f3af5cb31f315c27079ae71b02fcb671a7ef
adolfo <agonzalez15@mail.greenriver.edu>
2019-01-13 16:48:57 -0500

    created js file

---
+++ fizzbuzz.js
@@ -0,0 +1,23 @@
+/** Author: Adolfo Gonzalez
+    Date: 1/13/2019
+
+    File: fizzbuzz.js
+
+    This is the javascript file with the functions for js-review.html.
+ */
+//Create a JavaScript program that prints "Introduction to JavaScript"
+// to the console window,and test it in your browser.
+function jsIntro(){
+
+    console.log("Introduction to JavaScript");
+}
+jsIntro();
+//Write the FizzBuzz program described on page 38 of your text.
+
+//Write just the countBs function (and the code needed to test it)
+// described on page 56 ("Bean Counting") in your text.
+
+//Write the countChar function (and the code needed to test it)
+// described on page 56 ("Bean Counting") in your text.
+
+//Rewrite countBs to use countChar.
