f1bbb0f18e1e09d559baaa6f15e4952e0b6067f2
adolfo <agonzalez15@mail.greenriver.edu>
2019-01-13 21:38:15 -0500

    added char count function

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -6,20 +6,14 @@
     This is the javascript file with the functions for js-review.html.
  */
 
-
-//call functions
-jsIntro();
-fizzBuzz();
-
 /*  Create a JavaScript program that prints "Introduction to JavaScript"
     to the console window,and test it in your browser.*/
-function jsIntro(){
-    //print to console
-    console.log("Introduction to JavaScript");
-}
+console.log("Introduction to JavaScript");//first test to test console output
 
-//call function variable
-jsIntro();
+//function calls
+fizzBuzz();
+countBs('Blue, Black, Brown, Orange, Green, Yellow');
+countChars('blah,blah,blah,Blah,Bla,Bla', 'a');
 
 /*  Write the FizzBuzz program described on page 38 of your text. */
 
@@ -38,7 +32,7 @@ function fizzBuzz(){
             console.log("Buzz" );
         }else if ((i % 3) === 0){
 
-        //if numbers divisible by 3
+            //if numbers divisible by 3
             console.log("Fizz");
         }
         //
@@ -47,14 +41,55 @@ function fizzBuzz(){
             console.log(i);
         }
 
-
-    }
-}
+    }//end for loop
+}//end of fizzbuzz function
 
 /*  Write just the countBs function (and the code needed to test it)
     described on page 56 ("Bean Counting") in your text.*/
 
+/*function countBs(sentence){
+    //counter variable
+    var letter = 0;
+
+    for (var num = 0; num < sentence.length; num++){
+
+        if(sentence.charAt(num) === 'B'){
+
+            letter++;
+        }
+
+    }
+    console.log("The number of letter \"B\'s is " + letter);
+}*/
+
+
 /*  Write the countChar function (and the code needed to test it)
     described on page 56 ("Bean Counting") in your text.*/
 
+function countChars(sentence, letter){
+
+    //counter variable
+    var count = 0;
+
+    for (var num = 0; num < sentence.length; num++){
+
+        if(sentence.charAt(num) === letter){
+
+            count++;
+        }
+
+    }
+    console.log("There are " + count + ", " + letter + "\'s.");
+
+    //return count;
+}
+
 /*  Rewrite countBs to use countChar.*/
+
+function countBs(newSentence){
+
+    var rewrite = countChars(newSentence, 'B');
+
+    return rewrite;
+
+}
